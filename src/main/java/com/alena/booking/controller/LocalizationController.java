da2443ec73af004beffc335d455c925caec9cc91
Vadim Fridman <vadimfr@matrix.co.il>
2026-08-04 13:45:11 +0300

    geo location button design

--- src/main/java/com/alena/booking/controller/LocalizationController.java
+++ src/main/java/com/alena/booking/controller/LocalizationController.java
@@ -55,7 +55,8 @@ public class LocalizationController {
                 "cancelBooking",
                 "confirmCancelBooking",
                 "bookingCancelled",
-                "nameRequired"
+                "nameRequired",
+                "location"
 
         };
 
