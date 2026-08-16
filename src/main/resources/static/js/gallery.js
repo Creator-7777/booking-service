83b9efb9676f15e39d2b5912446d6b3a1e9b070f
Vadim Fridman <vadimfr@matrix.co.il>
2026-08-16 20:26:26 +0300

    work images update

--- src/main/resources/static/js/gallery.js
+++ src/main/resources/static/js/gallery.js
@@ -15,18 +15,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const galleryImages = [
 
-    "https://i.imgur.com/LSExOos.jpeg",
-    "https://i.imgur.com/5ukYEXt.jpeg",
-    "https://i.imgur.com/htjjFDI.jpeg",
-    "https://i.imgur.com/HmUITUZ.jpeg",
-    "https://i.imgur.com/edWSIk9.jpeg",
-    "https://i.imgur.com/iVw4fPs.jpeg",
-    "https://i.imgur.com/0wxEGTi.jpeg",
-    "https://i.imgur.com/8e1IrkP.jpeg",
-    "https://i.imgur.com/WNC5PDQ.jpeg",
-    "https://i.imgur.com/lKq22XZ.jpeg",
-    "https://i.imgur.com/kdGvPSp.jpeg",
-    "https://i.imgur.com/cTv15uz.jpeg"
+    "https://i.imgur.com/ECiQ2tM.jpeg",
+    "https://i.imgur.com/dcJP0lL.jpeg",
+    "https://i.imgur.com/ccL9wOZ.jpeg",
+    "https://i.imgur.com/CfC7wdG.jpeg",
+    "https://i.imgur.com/wPCEmU0.jpeg",
+    "https://i.imgur.com/18iI40s.jpeg",
+    "https://i.imgur.com/sK5wDYr.jpeg",
+    "https://i.imgur.com/9LkwfxJ.jpeg",
+    "https://i.imgur.com/q9bS6H0.jpeg",
+    "https://i.imgur.com/0bOJ8cA.jpeg",
+    "https://i.imgur.com/b7ejfqU.jpeg",
+    "https://i.imgur.com/V230YYg.jpeg"
+
+//    "https://i.imgur.com/LSExOos.jpeg",
+//    "https://i.imgur.com/5ukYEXt.jpeg",
+//    "https://i.imgur.com/htjjFDI.jpeg",
+//    "https://i.imgur.com/HmUITUZ.jpeg",
+//    "https://i.imgur.com/edWSIk9.jpeg",
+//    "https://i.imgur.com/iVw4fPs.jpeg",
+//    "https://i.imgur.com/0wxEGTi.jpeg",
+//    "https://i.imgur.com/8e1IrkP.jpeg",
+//    "https://i.imgur.com/WNC5PDQ.jpeg",
+//    "https://i.imgur.com/lKq22XZ.jpeg",
+//    "https://i.imgur.com/kdGvPSp.jpeg",
+//    "https://i.imgur.com/cTv15uz.jpeg"
 
     ];
 
